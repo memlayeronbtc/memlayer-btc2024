dbe8c268c17ecd0890101b8f23c120685b07f477
jackie lee <jackylee0424@users.noreply.github.com>
2024-07-10 14:49:45 -0700

    add alys rpc

--- evm-contracts/hardhat.config.js
+++ evm-contracts/hardhat.config.js
@@ -28,9 +28,14 @@ module.exports = {
       url: "http://127.0.0.1:8545",
     },
     rskTestnet: {
-      url: "https://rpc.testnet.rootstock.io/Obi2kMscdIOpJhldMFjo5AKvDS2Qa3-T",
+      url: `https://rpc.testnet.rootstock.io/${process.env.RSKTESTNET_RPC_KEY}`,
       chainId: 31,
       gasPrice: 60000000,
+      accounts:[`0x${process.env.DEPLOYER_PKEY}`]
+    },
+    alysTestnet: {
+      url: "http://54.226.158.187/",
+      chainId: 212121,
       accounts:[`0x${process.env.DEPLOYER_PKEY}`]
     }
   }
