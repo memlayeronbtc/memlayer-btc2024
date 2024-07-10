b6556e1e0034f356fbf1f26423e5a1656b6568d4
jackie lee <jackylee0424@users.noreply.github.com>
2024-07-10 14:51:38 -0700

    Update deploy.js

--- evm-contracts/scripts/deploy.js
+++ evm-contracts/scripts/deploy.js
@@ -9,16 +9,18 @@ const envfile = require("envfile");
 const filename = "../.env";
 const parsedFile = envfile.parse(fs.readFileSync(filename));
 
-// npx hardhat run scripts\deploy.js --network rskTestnet
+// npx hardhat run scripts\deploy.js --network rskTestnet // non-turbo apeertopeer
+// npx hardhat run scripts\deploy.js --network alysTestnet // turbo MEMLAYER•TOKEN
 
 async function main() {
 
   const [deployer] = await ethers.getSigners();
+  console.log(``)
   console.log("The signer is set to:", deployer.address);
   const TestTokenContract =
     await hre.ethers.getContractFactory("MemlayerToken");
-  const name = "TEST•RUNE";
-  const symbol = "TES";
+  const name = "MEMLAYER•TOKEN";
+  const symbol = "MEM";
   // const url = "http://127.0.0.1:8188"; // for local test
   const url = parsedFile.SERVER_URI;
   const maxSupply = ethers.utils.parseEther("100000000"); // cap the maximum lifted runes for safety
@@ -29,6 +31,11 @@ async function main() {
   await TestToken.connect(deployer).setRuneDepositAddress(parsedFile.RUNE_DEPOSIT_ADDRESS);
 
   console.log(`${name} deployed to: ${TestToken.address}`);
+  console.log(``)
+  console.log(`remember to setup chain RPC info in firebase database for firebase functions and CCIP-read gateway server to work`)
+  console.log(``)
+  console.log(`ETH_OPERATOR_ADDRESS: ${parsedFile.ETH_OPERATOR_ADDRESS}`);
+  console.log(`RUNE_DEPOSIT_ADDRESS: ${parsedFile.RUNE_DEPOSIT_ADDRESS}`);
   
   parsedFile.TOKEN_ADDRESS = TestToken.address;
   console.log("Saving as TOKEN_ADDRESS to:", filename);
