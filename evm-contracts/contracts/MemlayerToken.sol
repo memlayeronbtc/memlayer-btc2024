a316690e118be757d7d31b3332a7619f3b155421
jackie lee <jackylee0424@users.noreply.github.com>
2024-07-13 16:26:54 -0700

    Update MemlayerToken.sol

--- evm-contracts/contracts/MemlayerToken.sol
+++ evm-contracts/contracts/MemlayerToken.sol
@@ -1,4 +1,7 @@
-//SPDX-License-Identifier: Unlicense
+//SPDX-License-Identifier: MIT license
+
+// Memlayer ERC20 token w. CCIP-read BTC rune balance
+
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
