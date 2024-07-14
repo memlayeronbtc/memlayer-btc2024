1cb318d1abadc7f5c213c980bb731b310ec89bd0
jackie lee <jackylee0424@users.noreply.github.com>
2024-07-14 11:26:21 -0700

    clean up and remove unused env constant

--- evm-contracts/scripts/deploy.js
+++ evm-contracts/scripts/deploy.js
@@ -9,8 +9,9 @@ const envfile = require("envfile");
 const filename = "../.env";
 const parsedFile = envfile.parse(fs.readFileSync(filename));
 
-// npx hardhat run scripts\deploy.js --network rskTestnet // non-turbo apeertopeer
-// npx hardhat run scripts\deploy.js --network alysTestnet // turbo MEMLAYER•TOKEN
+// npx hardhat run scripts\deploy.js --network rskTestnet // non-turbo
+// npx hardhat run scripts\deploy.js --network alysTestnet // turbo
+// npx hardhat run scripts\deploy.js --network awsaga // test
 
 async function main() {
 
@@ -20,26 +21,24 @@ async function main() {
   const TestTokenContract =
     await hre.ethers.getContractFactory("MemlayerToken");
   const name = "MEMLAYER•TOKEN";
-  const symbol = "MEM";
+  const symbol = name.substring(0,3);
   // const url = "http://127.0.0.1:8188"; // for local test
   const url = parsedFile.SERVER_URI;
-  const maxSupply = ethers.utils.parseEther("100000000"); // cap the maximum lifted runes for safety
+  const maxSupply = ethers.utils.parseEther("1000000"); // cap the maximum lifted runes for safety
   const TestToken = await TestTokenContract.deploy(name, symbol, maxSupply);
   await TestToken.deployed();
   await TestToken.connect(deployer).setGatewayUrl([url]);
   await TestToken.connect(deployer).setTokenOperator(parsedFile.ETH_OPERATOR_ADDRESS, true);
   await TestToken.connect(deployer).setRuneDepositAddress(parsedFile.RUNE_DEPOSIT_ADDRESS);
 
-  console.log(`${name} deployed to: ${TestToken.address}`);
-  console.log(``)
   console.log(`remember to setup chain RPC info in firebase database for firebase functions and CCIP-read gateway server to work`)
   console.log(``)
   console.log(`ETH_OPERATOR_ADDRESS: ${parsedFile.ETH_OPERATOR_ADDRESS}`);
   console.log(`RUNE_DEPOSIT_ADDRESS: ${parsedFile.RUNE_DEPOSIT_ADDRESS}`);
-  
-  parsedFile.TOKEN_ADDRESS = TestToken.address;
-  console.log("Saving as TOKEN_ADDRESS to:", filename);
-  fs.writeFileSync(filename, envfile.stringify(parsedFile));
+
+  console.log(`----`)
+  console.log(`${name} deployed to: ${TestToken.address}`);
+  console.log(`----`)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
